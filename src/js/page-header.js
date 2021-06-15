737d19c8cddb5fb1cc8b192da0a13c9703e58bdc
Egrik <egor-office-mail@yandex.ru>
2021-06-15 10:43:17 +0300

    Полностью завершает стилизацию блока с банерами

--- src/js/page-header.js
+++ src/js/page-header.js
@@ -1,12 +1,15 @@
 const navOpenBtnElement = document.querySelector(`.page-header__open-btn`);
 const navElement = document.querySelector(`.page-header__nav`);
+const bodyElement = document.querySelector(`body`);
 
 navOpenBtnElement.addEventListener(`click`, () => {
   navElement.classList.add(`page-header__nav--open`);
+  bodyElement.style = `overflow: hidden;`;
 });
 
 const navCloseBtnElement = document.querySelector(`.page-header__close-btn`);
 
 navCloseBtnElement.addEventListener(`click`, () => {
   navElement.classList.remove(`page-header__nav--open`);
+  bodyElement.style = ``;
 });
