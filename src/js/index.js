04de0b3adbdfe287b6f3994bb4771dc99fd050d9
Egrik <egor-office-mail@yandex.ru>
2021-07-02 07:03:01 +0300

    Выполняет первичную настройку каталога

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
+import { PageHeader } from "./page-header.js";
 import { Slider } from "./slider.js";
+
+new PageHeader();
 
 const sliderContainerElement = document.querySelector(
   `.product-slider__container`
