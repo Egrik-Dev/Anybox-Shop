969a8109dccbfda61a104778a9d33c06ee5a479c
egrik-dev <egor-office-mail@yandex.ru>
2022-01-01 15:15:49 +0000

    Updates

--- js/add-product.js
+++ js/add-product.js
@@ -1,3 +1,112 @@
 import { PageHeader } from "./page-header.js";
+import { createElement } from "./utils.js";
 
 new PageHeader();
+
+const parametersListElement = document.querySelector(
+  `[data-parameters="list"]`
+);
+const parametersBtnElement = document.querySelector(`[data-parameters="btn"]`);
+
+const clickAddBtn = () => {
+  const itemsQty = parametersListElement.children.length;
+  const parameterItemElement = createElement(
+    generateParametersInputs(itemsQty)
+  );
+
+  const delBtnElement = parameterItemElement.querySelector(
+    `[data-parameters="del-btn"]`
+  );
+
+  delBtnElement.addEventListener(`click`, (evt) => {
+    evt.target.parentElement.remove();
+  });
+
+  parametersListElement.append(parameterItemElement);
+};
+
+const generateParametersInputs = (itemsQty) => {
+  return `<li class="form-elements__item" data-parameters="item">
+  <input class="form-elements__input-text" type="text" name="parameter-key-${
+    itemsQty + 1
+  }" id="parameter--key-${itemsQty + 1}-field"
+    placeholder="Ключ" required>
+  <label class="visually-hidden" for="parameter--key-${
+    itemsQty + 1
+  }-field">Ключ параметра ${itemsQty + 1}</label>
+  <span class="form-elements__dash">-</span>
+  <input class="form-elements__input-text" type="text" name="parameter-value-${
+    itemsQty + 1
+  }"
+    id="parameter--value-${itemsQty + 1}-field" placeholder="Значение" required>
+  <label class="visually-hidden" for="parameter--key-1-field">Значение параметра ${
+    itemsQty + 1
+  }</label>
+  <button class="add-product__parameters-del-btn btn" data-parameters="del-btn"><span
+  class="visually-hidden">Удалить характеристику</span>
+  </button>
+</li>`;
+};
+
+parametersBtnElement.addEventListener(`click`, clickAddBtn);
+
+// Реализация подгрузки инпутов file
+
+let itemImageElement = document.querySelector(`[data-image="item"]`);
+let inputImageElement = itemImageElement.querySelector(`input[name="image"]`);
+const containerImageElement = document.querySelector(
+  `[data-image="container"]`
+);
+
+const MAX_IMAGES_QTY = 3;
+
+const generateInputFile = (sequence) => {
+  return `<li class="add-product__image-item add-product__image-item--new">
+  <div class="add-product__image-wrapper">
+    <label class="add-product__label-file" for="image-field-${sequence}"><span class="visually-hidden">Картинка</span>
+      <svg class="add-product__img-add">
+        <use xlink:href="img/sprite.svg#icon-plus"></use>
+      </svg>
+      <input class="form-elements__input-file" type="file" name="image" id="image-field-${sequence}"
+        placeholder="Выберите картинку" data-image-last="true">
+    </label>
+  </div>
+</li>`;
+};
+
+const generateCheckImg = () => {
+  return `<svg class="add-product__img-add" style="fill: #71af2e">
+  <use xlink:href="img/sprite.svg#icon-circle-check"></use>
+</svg>`;
+};
+
+const addFileListener = () => {
+  inputImageElement.addEventListener(`change`, addNewInputFile);
+};
+
+const removeFileListener = () => {
+  inputImageElement.removeEventListener(`change`, addNewInputFile);
+};
+
+const addCheckImg = () => {
+  const parentNode = itemImageElement.querySelector(`.add-product__label-file`);
+  const plusImg = parentNode.querySelector(`.add-product__img-add`);
+  const doneImg = createElement(generateCheckImg());
+  parentNode.replaceChild(doneImg, plusImg);
+};
+
+const addNewInputFile = () => {
+  removeFileListener();
+  addCheckImg();
+
+  const allInputs = document.querySelectorAll(`input[name="image"]`);
+
+  if (allInputs.length < MAX_IMAGES_QTY) {
+    itemImageElement = createElement(generateInputFile(allInputs.length + 1));
+    inputImageElement = itemImageElement.querySelector(`input[name="image"]`);
+    addFileListener();
+    containerImageElement.append(itemImageElement);
+  }
+};
+
+addFileListener();
--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,139 @@
 import { PageHeader } from "./page-header.js";
+import { createElement } from "./utils.js";
 
 new PageHeader();
+
+const DeliveryMethod = {
+  PICKUP: {
+    name: `pickup`,
+    cost: 0,
+  },
+  MSK: {
+    name: `deliveryMsk`,
+    cost: 300,
+  },
+  RUSSIA_POST: {
+    name: `russiaPost`,
+    cost: 100,
+  },
+};
+
+class Cart {
+  constructor() {
+    this.products = document.querySelectorAll(`.order__cart-item`);
+    this.deliveryInputs = document.querySelectorAll(
+      `input[name="deliveryMethod"]`
+    );
+    this.deliveryMethod = document.querySelector(
+      `input[name="deliveryMethod"]:checked`
+    ).value;
+    this.deliveryContainer = document.querySelector(
+      `.order__delivery-adress-list`
+    );
+    this.deliveryCostElement = document.querySelector(
+      `[data-confirmation="deliveryCost"]`
+    );
+    this.productsPriceElement = document.querySelector(
+      `[data-confirmation="productsPrice"]`
+    );
+    this.totalPriceElement = document.querySelector(
+      `[data-confirmation="totalPrice"]`
+    );
+    this.totalPriceInput = document.querySelector(`input[name="totalPrice"]`);
+    this.deliveryPriceInput = document.querySelector(
+      `input[name="deliveryPrice"]`
+    );
+    this.postMsgElement = document.querySelector(
+      `.order__confirmation-post-msg`
+    );
+
+    this.changeDeliveryMethod = this.changeDeliveryMethod.bind(this);
+
+    this.deliveryInputs.forEach((input) => {
+      input.addEventListener(`change`, this.changeDeliveryMethod);
+    });
+  }
+
+  init() {
+    this.renderDeliveryfields(this.deliveryMethod);
+  }
+
+  changeDeliveryMethod(evt) {
+    this.deliveryMethod = evt.target.value;
+    this.renderDeliveryfields(this.deliveryMethod);
+  }
+
+  generateAdressField() {
+    return `<li class="form-elements__item form-elements__item--column">
+    <label class="form-elements__label-text" for="adress-field">
+      <span class="form-elements__label-text-title">Адрес</span>
+      <span class="form-elements__label-text-required">*</span>
+    </label>
+    <input class="form-elements__input-text" type="text" name="adress" id="adress-field" required>
+  </li>`;
+  }
+
+  generatePostField() {
+    return `<li class="form-elements__item form-elements__item--column">
+    <label class="form-elements__label-text" for="zip-code-field">
+      <span class="form-elements__label-text-title">Почтовый индекс</span>
+      <span class="form-elements__label-text-required">*</span>
+    </label>
+    <input class="form-elements__input-text" type="number" name="zipСode" id="zip-code-field" required>
+  </li>`;
+  }
+
+  generateCityField() {
+    return `<li class="form-elements__item form-elements__item--column">
+    <label class="form-elements__label-text" for="city-field">
+      <span class="form-elements__label-text-title">Город</span>
+      <span class="form-elements__label-text-required">*</span>
+    </label>
+    <input class="form-elements__input-text" type="text" name="city" id="city-field" required>
+  </li>`;
+  }
+
+  renderDeliveryfields(deliveryMethod) {
+    this.deliveryContainer.innerHTML = ``;
+    this.postMsgElement.classList.add(`visually-hidden`);
+
+    switch (deliveryMethod) {
+      case DeliveryMethod.PICKUP.name:
+        this.deliveryCostElement.innerHTML = `${DeliveryMethod.PICKUP.cost} ₽`;
+        break;
+      case DeliveryMethod.MSK.name:
+        this.deliveryContainer.append(
+          createElement(this.generateAdressField())
+        );
+        this.deliveryCostElement.innerHTML = `${DeliveryMethod.MSK.cost} ₽`;
+        break;
+      case DeliveryMethod.RUSSIA_POST.name:
+        this.deliveryContainer.append(createElement(this.generatePostField()));
+        this.deliveryContainer.append(createElement(this.generateCityField()));
+        this.deliveryContainer.append(
+          createElement(this.generateAdressField())
+        );
+        this.deliveryCostElement.innerHTML = `${DeliveryMethod.RUSSIA_POST.cost} ₽`;
+        this.postMsgElement.classList.remove(`visually-hidden`);
+        break;
+    }
+
+    this.calculateTotalPrice();
+  }
+
+  calculateTotalPrice() {
+    const productsPrice = parseInt(this.productsPriceElement.innerHTML, 10);
+    const deliveryPrice = parseInt(this.deliveryCostElement.innerHTML, 10);
+    const totalPrice = productsPrice + deliveryPrice;
+    this.totalPriceElement.innerHTML = `${totalPrice} ₽`;
+    this.fillPriceInputs(totalPrice, deliveryPrice);
+  }
+
+  fillPriceInputs(totalPrice, deliveryPrice) {
+    this.totalPriceInput.value = totalPrice;
+    this.deliveryPriceInput.value = deliveryPrice;
+  }
+}
+
+const cart = new Cart();
+cart.init();
